029b764b2bdbadc29c330d01c7ed0054cce77189
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-08-05 03:54:52 +0530

    Update sha3.js

--- sha3.js
+++ sha3.js
@@ -5,3 +5,14 @@ const webs3 = new Web3(infura_ws);
 _event = "Transfer(address,address,uint)";
 _eventutils = webs3.utils.sha3(_event);
 console.log(_eventutils);
+
+
+webs3.eth.subscribe('logs', {
+    address: '',
+    topics: [''],
+    fromBlock: 'latest',
+}, (error, log) {
+    if(!error) {
+        console.log(log)
+    }
+})
