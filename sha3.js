403787d920a572c9b55c68a4ccb5a305fc750047
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-08-05 03:48:42 +0530

    Update sha3.js

--- sha3.js
+++ sha3.js
@@ -1,5 +1,5 @@
 const Web3 = require("web3");
-const infura_ws = "wss://mainnet.infura.io/ws/v3/bfb05af0e44e44018a16c54c5a428de4";
+const infura_ws = "wss://mainnet.infura.io/ws/v3/b5af0e6c8de4";
 const webs3 = new Web3(infura_ws);
 
 _event = "Transfer(address,address,uint)";
